cb4a881b519cebf6afaf6043f8062f2290152220
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-02-07 07:16:00 +0100

    feature:axios call on questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { Question } from './interfaces';
+import axios from 'axios';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [questions, setQuestions] = useState<Question[]>([]);
 
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      const response = await axios.get(
+        `https://the-trivia-api.com/api/questions?limit=5`
+      );
+      setQuestions(response.data);
+    };
+    fetchQuestions();
+  }, []);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+      {questions
+        ? questions.map((question, key) => {
+            return <div key={key}>{question.question}</div>;
+          })
+        : null}
     </div>
-  )
+  );
 }
-
-export default App
