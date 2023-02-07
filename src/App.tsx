66a9b6578539ebff392b80518539e35bc3ec5b6b
Thomas_DCL_USR <thomas.dicola@user-agency.com>
2023-02-07 08:26:32 +0100

    feature quizz done, next step : put it in a random position

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,51 @@ import './App.css';
 
 export default function App() {
   const [questions, setQuestions] = useState<Question[]>([]);
+  const [correctAnswer, setCorrectAnswer] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchQuestions = async () => {
       const response = await axios.get(
-        `https://the-trivia-api.com/api/questions?limit=5`
+        `https://the-trivia-api.com/api/questions?limit=1`
       );
       setQuestions(response.data);
     };
     fetchQuestions();
-  }, []);
+  }, [correctAnswer]);
+
   return (
     <div className='App'>
       {questions
         ? questions.map((question, key) => {
-            return <div key={key}>{question.question}</div>;
+            return (
+              <div key={key}>
+                <h1>{question.category}</h1>
+
+                <h2>{question.question}</h2>
+
+                <ul>
+                  <div>
+                    <li>
+                      <button onClick={() => setCorrectAnswer(!correctAnswer)}>
+                        {question.correctAnswer}
+                      </button>
+                    </li>
+                  </div>
+
+                  <div>
+                    {question.incorrectAnswers.map((incorrect, key) => {
+                      return (
+                        <li key={key}>
+                          <button>{incorrect}</button>
+                        </li>
+                      );
+                    })}
+                  </div>
+                </ul>
+              </div>
+            );
           })
-        : null}
+        : 'Loading...'}
     </div>
   );
 }
